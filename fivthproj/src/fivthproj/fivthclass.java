bc4c0c96463ec5f456ba6abaaf9a321b04f32950
ganeshmamidi123 <77326020+ganeshmamidi123@users.noreply.github.com>
2021-02-03 16:13:09 +0530

    hj

--- fivthproj/src/fivthproj/fivthclass.java
+++ fivthproj/src/fivthproj/fivthclass.java
@@ -2,7 +2,7 @@ package fivthproj;
 
 public class fivthclass {
 	public static void main(String args []) {
-		System.out.println("this is the fivth class121");
+		System.out.println("this is the fivth class12");
 	}
 
 }
